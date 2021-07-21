be027f66edefea9195a0fc76af5e489aefe39830
Fernanda Kobs <75281337+nandakobs@users.noreply.github.com>
2021-07-21 20:02:49 -0300

    Add files via upload

---
+++ CFB Cursos/21.  Argumentos para a função main, argc e argv.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <string.h> //para fazer a comparação de string
+#include <stdlib.h>
+
+using namespace std;
+
+    /* Quando digitamos o nome do programa
+    no prompt por exemplo, eu posso enviar
+    um valor e esse valor pode ser tratado
+    aqui dentro do programa através de 2
+    argumentos padrões que são o argc e o argv
+
+    Na função main nós temos aqui disponivel
+    duas variaveis q vão servir de propriedades
+    q são padrões, a primeira é uma inteira
+    que se chama argc q vai guardar a
+    quantidade de parametros informados.
+    E o segundo é um parametro do tipo char
+    que ele é um ponteiro chamado argv q tbm
+    é uma matriz*
+    Basicamente cada elemento dessa matriz
+    quando eu digitar um argumento
+    ele vai apontar pro argumento digitado e
+    vai ficar armazenado nessa matriz  */
+
+int main(int argc, char *argv[]){
+  // o 1° armazena quantidade de argumentos
+  // o 2° armazena os argumentos
+
+  /*quando utilizamos argc e argv no main
+  ele automaticamente faz com q o primeiro
+  parametro/argumento seja o nome do programa. */
+
+ // cout << argv[0] << "\n\n";
+  /* sendo assim argv[1] [aka a segunda posição
+  pq matriz começa em 0] imprime o 1° valor q
+  vc digitar
+    */
+//fessor usando cmd na aula
+
+if(argc>1){
+ //temos que negar strcmp pq o valor Zero é false no IF
+    if(!strcmp(argv[1], "sol")){
+    /* strcmp faz uma comparação entre 2 strings
+     se as strings forem iguais retorna o valor 0/positivo
+     se diferentes ele retorna maior ou menor q zero
+     maior q zero quando a 1° string for maior q a 2°
+     menor q zero quando a 1° string for menor q a 2° */
+     cout << "Vou ao clube. \n\n";
+    }else if(!strcmp(argv[1], "nublado")){
+        cout << "Vou ao cinema. \n\n";
+    }else{
+        cout << "Vou ficar em casa. \n\n";
+        /*ou seja, se o usuario n digitar
+        nem sol nem nublado, imprimi ir pra casa */
+    }
+}
+
+    system("pause");
+
+	return 0;
+}
---
+++ CFB Cursos/22. Funções - Parte 1.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+//temos que prototipar/apresentar a função primeiro e embaixo escreve-las
+
+void texto();
+void soma(int n1, int n2);
+int soma2(int n1, int n2);
+void tr(string tra[4]);
+
+int main(){//main é uma função
+
+    string transp[4]=("carro","moto","barco","aviao");
+
+    int res;
+    res=soma2(175,25);
+    cout << "Valor de res: " << res << "\n";
+    //é o mesmo que
+    cout << "Valor de res: " << soma2(175,25) << "\n";
+
+    tr(transp);
+
+    texto(); //chamando a função
+
+    soma(15,5);
+
+    /*for(int i=0; i<10; i++){ //aqui escrevemos texto 10 vezes
+        texto();
+    }*/
+	return 0;
+}
+
+//escreva o código das funções dps do principal
+//a função só será executada quando ela for chamada no main
+//função void não retorna nenhum valor
+void texto(){
+    cout << "\nCanal Fessor Bruno\n";
+}
+
+void soma(int n1, int n2){
+    cout << "soma dos valores: " << n1+n2 << "\n";
+}
+
+//qualquer outra função escrita aqui temos q dar o valor a ser retornado
+
+int soma2(int n1, int n2){
+    return n1+n2;
+}
+
+void tr(string tra[4]){
+    for(int i=0; i<4; i++){
+        cout << tra[i] << "\n";
+    }
+}
