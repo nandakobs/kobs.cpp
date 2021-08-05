a27b198c317ad48ab583f628843f367606af63e4
Fernanda Kobs <75281337+nandakobs@users.noreply.github.com>
2021-08-05 16:02:27 -0300

    Add files via upload

---
+++ CFB Cursos/23. Sobrecarga de funções.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+void soma(int n1, int n2);
+void soma();
+
+int main(){
+	/*Sobrecarga de funções nada mais é do que a
+	possibilidade de ter duas ou mais funções
+	com o mesmo nome dentro do mesmo programa
+	- desde que as funções possum argumentos de
+	entrada/variaveis diferentes*/
+
+    soma(20,30);
+    soma();
+
+	return 0;
+}
+
+void soma(int n1, int n2){
+    int res;
+    res=n1+n2;
+    cout << "\nSoma de " << n1 << " com " << n2 << " = " << res << "\n";
+}
+
+
+void soma(/* ARGUMENTOS DE ENTRADA ficam aqui*/){
+    int n1, n2, res;
+    n1=10;
+    n2=20;
+    res=n1+n2;
+    cout << "\nSoma de " << n1 << " com " << n2 << " = " << res << "\n";
+
+}
---
+++ CFB Cursos/24.  Omissão de argumentos e argumentos padrão.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void imp(string txt="youtube");
+
+/*Para omitir o argumento, na linha do prototipo
+da função, atribua um valor para o argumento.
+Se ambos (chamada da função e prototipo n tiverem
+um valor n vai imprimir nada.
+O valor atribuido no prototipo só será impresso, se
+o da chamada estiver vazio*/
+
+int main(){
+
+    imp("canal");
+
+	return 0;
+}
+
+void imp(string txt){
+    cout << "\n" << txt << "\n";
+}
