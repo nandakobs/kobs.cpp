0edb131e2190785c9eedb89f3182d51f5376ebdf
Fernanda Kobs <75281337+nandakobs@users.noreply.github.com>
2021-08-05 22:54:34 -0300

    Add files via upload

---
+++ CFB Cursos/25. Funções recursivas (recursividade).cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+/*Sempre que eu tiver uma função que chama a si
+	mesma, isso se chama recursividade
+*/
+
+//SEM recursividade
+/*void contador (int num);
+
+int main(){
+
+	contador(10);
+
+	return 0;
+}
+
+void contador (int num){
+    for(int i=0; i<=num; i++){
+        cout << i << "\n";
+    }
+
+}
+*/
+
+//COM recursividade
+
+void contador (int num, int cont=0);
+
+int main(){
+
+	contador(20);
+	//veja bem aqui tamo determinando o valor de num
+
+	return 0;
+}
+
+void contador (int num, int cont){
+     cout << cont << "\n";
+     if(num > cont){
+        contador(num, ++cont);
+     }
+}
---
+++ CFB Cursos/26. Calculadora Fatonacci com funções recursivas.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int fatorial(int n);
+int fibonacci(int n);
+
+int main(){
+
+	int op, valor, res;
+	char sair;
+    start:
+	cout << "Seja bem-vinde ao Fatonacci! \nGostaria de realizar qual operacao? \n[1]Fatoracao [2]Fibonacci\n";
+    cin >> op;
+    system("cls");
+    if(op==1){
+        cout << "Calculadora de Fatoriais Iniciada...\n(insira um numero)\n";
+        cin >> valor;
+        res=fatorial(valor);
+        system("cls");
+        cout << "O fatorial de " << valor << " tem valor de " << res << endl;
+    }else if(op==2){
+        cout << "Sequencia de Fibonacci Iniciada...\n(insira um valor)\n";
+        cin >> valor;
+        system("cls");
+        cout << "Fibonacci com " << valor << " valores: \n";
+        for(int i=0; i<valor; i++){
+            cout << fibonacci(i+1) << "  " << endl;
+        }
+    }else {
+        cout << "Operacao invalida!\n";
+
+    }
+    system("pause");
+    system("cls");
+    cout << "Deseja fazer mais alguma operacao?[s/n]";
+    cin >> sair;
+    if(sair=='S' || sair=='s'){
+        system("cls");
+        goto start;
+    }else {
+    system("exit");
+    }
+
+
+	return 0;
+}
+
+int fatorial (int n){
+    //4!=4*3*2*1
+    if(n==0){
+        return 1;
+    }
+    return n*fatorial(n-1);
+}
+
+int fibonacci (int n){
+    //0,1,(0+1)1,(1+1)2,(1+2)3;(2+3)5,(3+5)8,...
+    if(n==1 || n==2){
+        return 1;
+    }else{
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
+
+}
