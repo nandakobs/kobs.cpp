991a8d6c60ba14595d10867b1bd75083d1ddff31
Fernanda Kobs <75281337+nandakobs@users.noreply.github.com>
2021-08-06 21:56:31 -0300

    Add files via upload

---
+++ CFB Cursos/27. Enum.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	/* Enum(Enumeração)  é um conjunto de constantes
+	inteiras que espeficicam valores que uma variavel
+	desse tipo pode ter. Estrutura de dados com valores
+	numericos.*/
+	//Digamos que queremos saber o quanto de munição cada arma tem
+	enum armas{fuzil=100, revolver=8, rifle=12, escopeta=1};
+	/*Por padrão cada item do enum já possui um valor.
+    O padrão é que ele adiciona zero pro primeiro elemento
+    e sempre vai em uma estrutura sequencial.
+    Ou seja: 0, 1, 2, 3...
+    Se eu definir apenas que fuzil=100 então: revolver será 101
+    rifle será 102 e assim por diante
+    */
+    //Criando uma variavel
+    int num; // tipo inteiro, nome num
+    armas armaSel; // tipo armas, nome armaSel (selecionada)
+    // os valores possiveis pra a variavel acima são os itens de enum armas
+
+    armaSel=rifle;
+    cout << armaSel; //Pelo valor padrão imprime n°2, pelo valor dado imprime 12
+	return 0;
+}
---
+++ CFB Cursos/28-29.  Pilha Stack.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <stack> //pra trabalhar com pilhas
+using namespace std;
+
+int main(){
+	/*Pilha é como se fosse um vetor só que tem controles
+	especificos. Pode-se pensar num poço(ou tubo com só uma
+    abertura), eu coloco elementos dentro desse tubo.
+    O primeiro elemento a ser inserido é o ultimo elemento
+    a ser removido ou trabalhado. O ultimo elemento
+    a ser inserido é o primeiro elemento a ser removido
+    ou trabalhado.*/
+
+    stack <string> cartas;
+    // imagine q tenho um baralho e quero formar um monte
+    // metodo push insere elementos na pilha
+    cartas.push("Rei de Copas");
+    cartas.push("Rei de Espadas");
+    cartas.push("Rei de Ouros");
+    cartas.push("Rei de Paus"); //topo da pilha pois é o ultimo add
+
+    cout << "Tamanho da pilha: " << cartas.size() << "\n";
+    // metodo top revela elemento q está no topo
+    cout << "Carta do topo: " << cartas.top() << "\n";
+    //metodo pop retira elementos da pilha (do topo)
+    cartas.pop();
+
+    cout << "Tamanho da pilha: " << cartas.size() << "\n";
+    cout << "Nova carta do topo: " << cartas.top() << "\n";
+
+    //metodo empty verifica se a pilha está vazia, se sim true, se não false
+    if(cartas.empty()){
+        cout << "Pilha vazia\n\n";
+    }else{
+        cout << "Pilha com cartas\n\n";
+    }
+    //Se essa verificação estivesse em antes da declaração dos itens(cartas) então estaria vazia
+
+    /* Outra forma de verificar se a pilha está vazia seria:
+    if(cartas.size() == 0){
+        cout << "Pilha vazia\n\n";
+    }else{
+        cout << "Pilha com cartas\n\n";
+    }
+    */
+
+    //Posso zerar tds os elementos ou esvaziar a pilha com:
+    while(!cartas.empty()){
+        // ou seja, enquanto cartas NAO estiver vazio
+        cartas.pop();
+    }
+     cout << "Tamanho da pilha: " << cartas.size() << "\n";
+
+
+
+
+}
+
