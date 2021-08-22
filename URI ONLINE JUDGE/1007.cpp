dfc253649abff3ab7391b7154e5e25a4abf95527
Fernanda Kobs <75281337+nandakobs@users.noreply.github.com>
2021-08-22 00:05:49 -0300

    Add files via upload

---
+++ URI ONLINE JUDGE/1007.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	int A, B, C, D, res;
+	cin >> A;
+	cin >> B;
+	cin >> C;
+	cin >> D;
+	res = (A * B - C * D);
+	cout << "DIFERENCA = " << res << endl;
+
+	return 0;
+}
